c837f3d8a0b9343eaa397c9964d1adfcdcb087c5
Ha Meem <hameem812@gmail.com>
2025-06-08 03:42:55 +0600

    patient profile update works

--- backend/MediLine/src/main/java/com/example/MediLine/Controller/AuthenticationController.java
+++ backend/MediLine/src/main/java/com/example/MediLine/Controller/AuthenticationController.java
@@ -112,7 +112,39 @@ public class AuthenticationController {
             String email = authentication.getName();
             Patient patient = patientRepository.findByEmail(email)
                     .orElseThrow(() -> new IllegalArgumentException("No Patient found with this email."));
-            return ResponseEntity.ok(patient);
+            PatientProfileData responseBody = new PatientProfileData(
+                    patient.getEmail(),
+                    patient.getFirstName(),
+                    patient.getLastName(),
+                    patient.getGender(),
+                    patient.getDateOfBirth(),
+                    patient.getBloodGroup(),
+                    patient.getPhoneNumber(),
+                    patient.getAddress(),
+                    patient.getProfilePhotoUrl()
+            );
+            return ResponseEntity.ok(responseBody);
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+    }
+
+    @PostMapping("/patient/profile/update")
+    public ResponseEntity<String> updatePatientProfile(@RequestBody PatientProfileData patientProfileData){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            String email = authentication.getName();
+            Patient patient = patientRepository.findByEmail(email)
+                    .orElseThrow(() -> new IllegalArgumentException("No Patient found with this email."));
+            patient.setFirstName(patientProfileData.getFirstName());
+            patient.setLastName(patientProfileData.getLastName());
+            patient.setGender(patientProfileData.getGender());
+            patient.setDateOfBirth(patientProfileData.getDateOfBirth());
+            patient.setBloodGroup(patientProfileData.getBloodGroup());
+            patient.setPhoneNumber(patientProfileData.getPhoneNumber());
+            patient.setAddress(patientProfileData.getAddress());
+            patient.setProfilePhotoUrl(patientProfileData.getProfilePhotoUrl());
+            patientRepository.save(patient);
+            return ResponseEntity.ok("Profile Updated Successfully.");
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
     }
